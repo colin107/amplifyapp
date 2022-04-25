d12a646bff2c451d32cb2370a20a9715d8124bb3
colin_kl_wong <colin_kl_wong@hkpo.gov.hk>
2022-04-25 17:46:43 +0800

    api

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,48 @@
 // import logo from '../logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 function App() {
-  const word = 'React';
+  const [count, setCount] = useState(1);
+  const [lists, setLists] = useState([])
+
+  console.log("count",count)
+  console.log("lists",lists)
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/?page=${count}`)
+    .then(res => res.json())
+    .then(json => setLists(json))
+  }, [count])
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Colin Wong</h1>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
+        <button onClick={() => setCount(count+1)}>
+          Next
+        </button>
+
+        {/* <ul>
+          {
+
+              lists.results.map(
+                item => (
+                  <p key={item.id}>{item.name}</p>
+                )
+              )
+
+          }
+        </ul> */}
       </header>
-
       <body className="App-body">
-
       </body>
     </div>
-    // <h1>Hello, {word}!</h1>
-
   );
 }
 
