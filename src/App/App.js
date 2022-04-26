7f25c31baae0cb7c6c1804f75dc00190bc2e5a1c
colin_kl_wong <colin_kl_wong@hkpo.gov.hk>
2022-04-26 15:17:38 +0800

    add drawer

--- src/App/App.js
+++ src/App/App.js
@@ -2,19 +2,66 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 function App() {
   const [count, setCount] = useState(1);
   const [lists, setLists] = useState([])
-
-  console.log("count",count)
-  console.log("lists",lists)
-
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character/?page=${count}`)
     .then(res => res.json())
-    .then(json => setLists(json))
+    .then(json => setLists(json));
+    window.scrollTo(0, 0);
   }, [count])
 
 
@@ -41,21 +88,38 @@ function App() {
     )
   }
 
-
-
   return (
     <div className="App">
-
-      
       <header className="App-header">
-
       </header>
-      <body className="App-body">        
-        <ul>
-          {(lists.results!=null)?(HasResults()):(HasNoResults())}
-        </ul> 
+      <body className="App-body">     
 
-        <Button variant="contained" onClick={() => setCount(count+1)}>
+
+      <div>
+      {['left', 'right', 'top', 'bottom'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+
+
+
+
+
+
+          {(lists.results!=null)?(HasResults()):(HasNoResults())}
+
+        <Button variant="contained" onClick={
+          () => setCount(count+1)
+          }>
           Next
         </Button>
       </body>
